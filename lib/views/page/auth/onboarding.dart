636631342071d66b88e80b264dd1ef5dab0b70eb
antonio-pedro99 <tonio.pedro99@gmail.com>
2022-11-22 16:36:52 +0530

    upload files

--- lib/views/page/auth/onboarding.dart
+++ lib/views/page/auth/onboarding.dart
@@ -1,8 +1,10 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_storage_s3/amplify_storage_s3.dart';
 import 'package:flutter/material.dart';
 import 'package:space_client_app/views/page/auth/login.dart';
 import 'package:space_client_app/views/page/auth/signup.dart';
+import 'package:space_client_app/views/page/page_driver.dart';
 import 'package:space_client_app/views/theme/colors.dart';
 import 'package:space_client_app/views/widgets/button_text.dart';
 
@@ -26,9 +28,9 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
 
   Future<void> _configureAmplify() async {
     final auth = AmplifyAuthCognito();
-
+    final storage = AmplifyStorageS3();
     try {
-      await amplify.addPlugin(auth);
+      await amplify.addPlugins([auth, storage]);
       await amplify.configure(amplifyconfig);
       setState(() => _amplifyConfigured = true);
     } catch (e) {
@@ -77,7 +79,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
                           widget: MediaQuery.of(context).size.width * .6,
                           onTap: () => Navigator.of(context).pushAndRemoveUntil(
                               MaterialPageRoute(
-                                  builder: (context) => const LoginPage()),
+                                  builder: (context) => const PageDriver()),
                               (route) => false),
                         ),
                         CustomButton(
--- lib/views/page/functions.dart
+++ lib/views/page/functions.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
@@ -19,13 +22,45 @@ void openModalBottomSheet(BuildContext context, Widget content) {
 }
 
 void pickFileFromOs(BuildContext context) {
-  FilePicker.platform.pickFiles().then((result) {
+  FilePicker.platform.pickFiles().then((result) async {
     if (result != null) {
-      print(result.files);
+      final platformFile = result.files.single;
+      final path = platformFile.path!;
+      final key = platformFile.name;
+      final file = File(path);
+      try {
+        await Amplify.Storage.uploadFile(
+            local: file,
+            key: key,
+            options: UploadFileOptions(
+                accessLevel: StorageAccessLevel.private, metadata: {}),
+            onProgress: (progress) {
+              showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                      content: SizedBox(
+                        height: 100,
+                        child: Column(
+                          children: [
+                            LinearProgressIndicator(
+                              value: progress.getFractionCompleted(),
+                            ),
+                            Text(progress.currentBytes.toString())
+                          ],
+                        ),
+                      ),
+                      title: const Text("Uploading your file"),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15))));
+            });
+        print("Uploaded the file");
+      } on StorageException catch (e) {
+        print("Error $e");
+      }
     } else {
       print("Cant");
     }
   });
 
-  Navigator.of(context).pop();
+  /* Navigator.of(context).pop(); */
 }
