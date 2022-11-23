24b478897ac921b7b9e9e82a83bf56b7849da10d
antonio-pedro99 <tonio.pedro99@gmail.com>
2022-11-23 10:01:32 +0530

    start user bloc

--- lib/blocs/user/user_state.dart
+++ lib/blocs/user/user_state.dart
@@ -4,3 +4,13 @@ part of 'user_bloc.dart';
 abstract class UserState {}
 
 class UserInitial extends UserState {}
+
+class UserLogged extends UserState {}
+
+class UserNotLogged extends UserState {}
+
+class UserSignedOut extends UserState {}
+
+class UserSessionExpired extends UserState {}
+
+class UserDeleted extends UserState {}
