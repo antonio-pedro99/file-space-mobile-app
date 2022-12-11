cc0718fca10656d57d7061ccf16c5223af304a77
antonio-pedro99 <tonio.pedro99@gmail.com>
2022-12-11 12:42:37 +0530

    clean code

--- lib/views/page/desktop/deskotps.dart
+++ lib/views/page/desktop/deskotps.dart
@@ -1,14 +1,9 @@
-import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:space_client_app/blocs/file/file_bloc.dart';
 import 'package:space_client_app/blocs/user/user_bloc.dart';
 import 'package:space_client_app/data/models/object.dart';
 import 'package:space_client_app/views/page/desktop/device_tile.dart';
-import 'package:space_client_app/views/page/functions.dart';
-import 'package:space_client_app/views/page/home/widgets/file_tile.dart';
 import 'package:space_client_app/views/theme/colors.dart';
-import 'package:space_client_app/views/widgets/sort_navigator.dart';
 
 class DesktopFilesPage extends StatefulWidget {
   const DesktopFilesPage({Key? key, required this.title, this.userEmail})
